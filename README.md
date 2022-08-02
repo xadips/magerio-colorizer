88857f096e34538b19aa64e4957939225ed78c07
spidax <29689012+xadips@users.noreply.github.com>
2022-08-02 19:50:45 +0300

    lets go

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Magerio Colorizer
+Adds a random color to a Magerio nickname
+
+## Environmental variables
+Add these variables to your .env file in the same folder as the magerio.py
+`DISCORD_TOKEN='Bot's discord token`
+`GUILD_ID='Your server's ID`
+`MAGER_ID='Your Mager's ID`
+
+## Running your own Magerio Colorizer
+Python 3.9+ just to be sure
+Best to use in a virtual environment ;)
+
+```bash
+python -m venv bot
+source bot/bin/activate
+pip install -r requirements.txt
+python magerio.py
+```
+
+## Contributing
+Any contributions are welcome, please create a PR and collaborate <3
--- magerio.py
+++ magerio.py
@@ -1,17 +1,27 @@
 import os
+import random
+import time
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands, tasks
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+token = os.getenv('DISCORD_TOKEN')
+guild_id = os.getenv('GUILD_ID')
+mager_id = os.getenv('MAGER_ID')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='.sus.')
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Potato')
+    guild = bot.get_guild(int(guild_id))
+    user = await guild.fetch_member(int(mager_id))
+    while True:
+        color = random.choice(open('colors.txt').readlines())
+        await user.edit(nick='Magerio' + ' ' + color)
+        time.sleep(86400)
 
 
-client.run(TOKEN)
+bot.run(token)
