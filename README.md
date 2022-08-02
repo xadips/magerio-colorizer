6848775adad3b165d2b1e20d623f9aa681ed600b
spidax <29689012+xadips@users.noreply.github.com>
2022-08-02 18:11:08 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Magerio Colorizer
---
+++ magerio.py
@@ -0,0 +1,17 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Potato')
+
+
+client.run(TOKEN)
